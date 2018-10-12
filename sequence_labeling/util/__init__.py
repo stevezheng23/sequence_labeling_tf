3ee14aace143b4de1f0248ad83882faab78de381
Mingzhi Zheng <mizheng@microsoft.com>
2018-10-12 10:59:40 -0700

    add eval util for sequence labeling

---
+++ sequence_labeling/util/__init__.py
@@ -0,0 +1,2 @@
+__all__ = ["debug_logger", "train_logger", "eval_logger.py", "summary_writer", "result_writer",
+           "default_util", "param_util", "data_util", "model_util", "eval_util", "layer_util", "contextual_modeling_util"]
---
+++ sequence_labeling/util/eval_util.py
@@ -0,0 +1,38 @@
+import numpy as np
+import tensorflow as tf
+
+from external.precision_recall import *
+
+__all__ = ["evaluate_from_data"]
+
+def _precision(pred_data, label_data):
+    """Precision"""
+    precision, _ = get_precision_recall(pred_data, label_data)
+    return precision
+
+def _recall(pred_data, label_data):
+    """Recall"""
+    _, recall = get_precision_recall(pred_data, label_data)
+    return recall
+
+def _f1_score(pred_data, label_data):
+    """F1 score"""
+    precision, recall = get_precision_recall(pred_data, label_data)
+    f1_score = 2 * precision * recall / (precision + recall)
+    return f1_score
+
+def evaluate_from_data(pred_data, label_data, metric):
+    """compute evaluation score based on selected metric"""
+    if len(pred_data) == 0 or len(label_data) == 0:
+        return 0.0
+    
+    if metric == "precision":
+        eval_score = _precision(pred_data, label_data)
+    elif metric == "recall":
+        eval_score = _recall(pred_data, label_data)
+    elif metric == "f1":
+        eval_score = _f1_score(pred_data, label_data)
+    else:
+        raise ValueError("unsupported metric {0}".format(metric))
+    
+    return eval_score
